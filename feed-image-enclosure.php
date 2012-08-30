0916e7ad4cb520d81db517027ae607e75184b3d0
Kaspars <kaspars@metronet.no>
2012-08-30 13:30:36 +0200

    add image size filter
    Signed-off-by: Kaspars <kaspars@metronet.no>

--- feed-image-enclosure.php
+++ feed-image-enclosure.php
@@ -9,16 +9,17 @@ Author URI: http://konstruktors.com
 License: GPL2
 */
 
-add_action( 'rss2_item', 'add_rss_featured_image_enclosure' );
+add_action( 'rss2_item', 'add_post_featured_image_as_rss_item_enclosure' );
 
-function add_rss_featured_image_enclosure() {
+function add_post_featured_image_as_rss_item_enclosure() {
 	global $post, $wp_query;
 
 	if ( ! has_post_thumbnail() )
 		return;
 
 	$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
-	$thumbnail = wp_get_attachment_image_src( $thumbnail_id, 'thumbnail' );
+	$thumbnail_size = apply_filters( 'rss_enclosure_image_size', 'thumbnail' );
+	$thumbnail = wp_get_attachment_image_src( $thumbnail_id, $thumbnail_size );
 
 	printf( 
 		'<enclosure url="%s" length="%s" type="%s" />',
