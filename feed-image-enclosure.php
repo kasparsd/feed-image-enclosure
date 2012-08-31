59d06a719dbf734cd029c5fbd359a4aa824511ab
Kaspars <kaspars@metronet.no>
2012-08-31 14:31:59 +0200

    return the correct file size for the specified image size
    Signed-off-by: Kaspars <kaspars@metronet.no>

--- feed-image-enclosure.php
+++ feed-image-enclosure.php
@@ -3,7 +3,7 @@
 Plugin Name: Feed Image Enclosure
 Plugin URI: https://github.com/kasparsd/
 Description: Add featured images as enclosures in RSS feeds
-Version: 0.1
+Version: 0.2
 Author: Kaspars Dambis
 Author URI: http://konstruktors.com
 License: GPL2
@@ -12,19 +12,22 @@ License: GPL2
 add_action( 'rss2_item', 'add_post_featured_image_as_rss_item_enclosure' );
 
 function add_post_featured_image_as_rss_item_enclosure() {
-	global $post, $wp_query;
-
 	if ( ! has_post_thumbnail() )
 		return;
 
-	$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
 	$thumbnail_size = apply_filters( 'rss_enclosure_image_size', 'thumbnail' );
-	$thumbnail = wp_get_attachment_image_src( $thumbnail_id, $thumbnail_size );
+	$thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+	$thumbnail = image_get_intermediate_size( $thumbnail_id, $thumbnail_size );
+
+	if ( empty( $thumbnail ) )
+		return;
+
+	$upload_dir = wp_upload_dir();
 
 	printf( 
 		'<enclosure url="%s" length="%s" type="%s" />',
-		$thumbnail[0], 
-		filesize( get_attached_file( $thumbnail_id ) ), 
+		$thumbnail['url'], 
+		filesize( path_join( $upload_dir['basedir'], $thumbnail['path'] ) ), 
 		get_post_mime_type( $thumbnail_id ) 
 	);
 }
